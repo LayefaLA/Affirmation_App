19dfd0677f37cd315f5a0fa60bb18b88ad4f79be
LayefaL <klagwosi@hotmail.co.uk>
2023-09-03 21:37:32 +0100

    First Group project

---
+++ Group2_AffirmationsApp/config.py
@@ -0,0 +1,8 @@
+HOST = "localhost"
+USER = "root"
+PASSWORD = "fT>Mo>f0MIEd"
+
+"""
+Poppy's mySQL details above. Please complete with your own to run.
+Please also open and run the SQL_set_up file in your SQL to set up the database and table.
+"""
